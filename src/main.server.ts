dd3ae25dd4ab339c54071ffe5f08658af762fe29
hung159 <tyghu105@gmail.com>
2023-12-23 13:45:59 +0700

    toi lam s=xong cau 2 nhe

--- src/main.server.ts
+++ src/main.server.ts
@@ -7,8 +7,10 @@ import { OrderComponent } from './app/order/order.component';
 import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
 import { LoginComponent } from './app/login/login.component'
 import { RegisterComponent } from './app/register/register.component'
+import { DetailProductComponent } from './app/detail-product/detail-product.component';
+
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(RegisterComponent, config);
+const bootstrap = () => bootstrapApplication(DetailProductComponent, config);
 
 export default bootstrap;
