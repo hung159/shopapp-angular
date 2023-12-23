a4c6d80ee59917ee68c9d286cf2a0be885453f96
hung159 <tyghu105@gmail.com>
2023-12-23 12:58:38 +0700

    done nhe

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [HeaderComponent, FooterComponent]
+})
+export class LoginComponent {
+
+}
--- src/main.server.ts
+++ src/main.server.ts
@@ -5,8 +5,10 @@ import { HeaderComponent } from './app/header/header.component';
 import { FooterComponent } from './app/footer/footer.component';
 import { OrderComponent } from './app/order/order.component';
 import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
+import { LoginComponent } from './app/login/login.component'
+import { RegisterComponent } from './app/register/register.component'
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(OrderConfirmComponent, config);
+const bootstrap = () => bootstrapApplication(RegisterComponent, config);
 
 export default bootstrap;
